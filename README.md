19e16374621cbbadaa55fb94db7049a8b1c45df7
Vicente Mundim <vicente.mundim@gmail.com>
2012-09-13 11:18:24 -0300

    Adding a security measure that ensures that a valid token is given when uploading files

--- README.md
+++ README.md
@@ -22,9 +22,23 @@ Finally start the server by:
 
 Besides the AWS key and secret you may also configure other aspects of the server, like specifying the hosts allowed to upload files, the server port, whether to use SSL or not for files, etc. Here are the environment variable that can be used:
 
-* AWS_REGION: The AWS region to be used (defaults to US_EAST_1)
-* AWS_POLICY: The policy to be used for uploaded files (defaults to public-read)
-* ALLOW_ORIGIN: A comman separated list of hosts that are allowed to upload files (Controls the Access-Control-Allow-Origin header. Defaults to '*')
-* REDISTOGO_URL: The URL of the redis server (Defaults to localhost:6379)
-* USE_SSL: Whether returned URLs should use HTTPS or HTTP
-* PORT: The server port
+* `AWS_REGION`: The AWS region to be used (defaults to US_EAST_1)
+* `AWS_POLICY`: The policy to be used for uploaded files (defaults to public-read)
+* `ALLOW_ORIGIN`: A comman separated list of hosts that are allowed to upload files (Controls the Access-Control-Allow-Origin header. Defaults to '*')
+* `REDISTOGO_URL`: The URL of the redis server (Defaults to localhost:6379)
+* `USE_SSL`: Whether returned URLs should use HTTPS or HTTP
+* `PORT`: The server port
+
+== Security
+
+You should probably think about how to prevent malicious user from uploading files into your S3 account once you've deployed the Cloud Uploader into your server. Since we try to be compatible with jQuery File Upload, we created a simple security measure that will prevent unwanted file uploads, while still being able to support uploads in IE.
+
+It works by verifying a hash generated with a secret that both the upload server and the application must know. The secret may be any value you want, only make sure it is well protected. It should be given to the application through environment variable `SECURITY_SECRET`. If it is not set, then the uploads will not be protected at all, and anyone that knows your server URL will be able to upload files to it.
+
+The hash should be generated with the following logic: the first 10 characters should be the current timestamp in seconds, then the next 10 characters a random number, finally the next 40 characters should be the SHA1 hex of the timestamp, the random number and the secret, concatenated.
+
+For example, suppose a timestamp of `12345667890`, and a random number of `9876543210`, with a secret of `aSecretPass`, the generated hash should be:
+
+    1234566789098765432108d128da83b95dd484381a15059e726898c80c8d3
+
+The timestamp will make sure that a malicious user can't use this hash over and over again, since it will only be valid by a small period of time. This can be configured using the `SECURITY_SECRET_EXPIRATION` environment variable. If it is not set, it will default to 600, which means that hashes will be expired in 5 minutes.
--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var express    = require('express'),
     amazon     = awssum.load('amazon/amazon'),
     S3         = awssum.load('amazon/s3').S3,
     fs         = require('fs'),
-    redis      = require('redis-url')
+    redis      = require('redis-url'),
+    crypto     = require('crypto'),
     _          = require("underscore")._;
 
 var s3 = new S3({
@@ -30,6 +31,9 @@ app.set('bucket', process.env.AWS_BUCKET);
 app.set('policy', process.env.AWS_POLICY);
 app.set('use-ssl', process.env.USE_SSL);
 app.set('upload-path', '/uploads/');
+
+app.set('secret', process.env.SECURITY_SECRET);
+app.set('secret_expiration', process.env.SECURITY_SECRET_EXPIRATION || 600); // 5 minutes of expiration
 
 app.set('port', process.env.PORT || 5000)
 
@@ -84,6 +88,21 @@ function handleResult(request, response, result, redirect) {
   }
 }
 
+function validToken(value) {
+  var secret           = app.get('secret'),
+      expiration       = app.get('secret_expiration')
+      currentTimestamp = Math.round(new Date().getTime()/1000)
+      timestamp        = parseInt(value.substr(0,  10), 10),
+      random           = value.substr(10, 10),
+      hash             = value.substr(20),
+      shasum           = crypto.createHash('sha1')
+
+  shasum.update('' + timestamp + random + secret)
+  digest = shasum.digest('hex')
+
+  return timestamp + expiration > currentTimestamp && hash == digest
+}
+
 // Configuration
 
 app.configure('development', function(){
@@ -108,7 +127,7 @@ app.get('/status', function(request, response) {
       if (!error) {
         response.jsonp({finished_uploading: redis_response == 0})
       } else {
-        response.jsonp(500, error)
+        response.jsonp(500, [{error: error}])
       }
     })
   } else {
@@ -125,7 +144,9 @@ app.post('/upload', function(request, response) {
   log('starting upload');
 
   // parse
-  var form = new formidable.IncomingForm(), files = {};
+  var form   = new formidable.IncomingForm(), files = {},
+      secret = app.get('secret'),
+      valid  = _(secret).isUndefined();
 
   form.maxFieldsSize = 20 * 1024 * 1024; // 20mb
   form.keepExtensions = true;
@@ -134,6 +155,11 @@ app.post('/upload', function(request, response) {
       expireUpload = 10 * 24 * 60 * 60; // 10 days
 
   form.on('file', function(name, file) {
+    if (!valid) {
+      log('Invalid token given, not uploading file to amazon')
+      return
+    }
+
     log("%s uploaded successfully, sending it to to the %s bucket in s3", file.name, app.get('bucket'));
 
     var startTime = new Date(),
@@ -170,6 +196,10 @@ app.post('/upload', function(request, response) {
 
   form.on('field', function(name, value) {
     log('field received: %s=%s', name, value);
+
+    if (secret && name == 'token') {
+      valid = validToken(value)
+    }
   });
 
   form.on('end', function() {
@@ -177,8 +207,12 @@ app.post('/upload', function(request, response) {
   });
 
   form.parse(request, function(err, fields, files) {
-    var filesInfo = _(files).map(function (file) { return new FileInfo(file) });
-    handleResult(request, response, filesInfo, fields.redirect)
+    if (valid) {
+      var filesInfo = _(files).map(function (file) { return new FileInfo(file) });
+      handleResult(request, response, filesInfo, fields.redirect)
+    } else {
+      response.jsonp(403, [{error: 'Forbidden'}])
+    }
   });
 });
 
