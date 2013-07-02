17066cbaa094ce5086bbbddb73edcc9b09eafa63
Vicente Mundim <vicente.mundim@gmail.com>
2013-07-02 12:05:40 -0300

    Adding log messages when sending data on pusher

--- app.js
+++ app.js
@@ -202,8 +202,10 @@ app.post('/upload', function(request, response) {
       fs.unlink(file.path);
 
       if (errors) {
+        log('amazon upload failed, triggering upload-failed on pusher')
         pusher.trigger(channelName, 'upload-failed', errors);
       } else {
+        log('amazon upload failed, triggering upload-completed on pusher')
         pusher.trigger(channelName, 'upload-completed', JSON.stringify(file));
       }
     });
